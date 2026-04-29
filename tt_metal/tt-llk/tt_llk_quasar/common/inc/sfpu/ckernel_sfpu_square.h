6e12da26371ef7799da7ff6aeecad4ee04f9d710
Shengxiang Ji <shengxiangji@tenstorrent.com>
2026-04-29 18:38:12 +0000

    builtin implementation of compiler optimizations

--- tt_metal/tt-llk/tt_llk_quasar/common/inc/sfpu/ckernel_sfpu_square.h
+++ tt_metal/tt-llk/tt_llk_quasar/common/inc/sfpu/ckernel_sfpu_square.h
@@ -7,6 +7,7 @@
 #include "ckernel_ops.h"
 #include "ckernel_trisc_common.h"
 #include "cmath_common.h"
+#include "lltt.h"
 
 namespace ckernel
 {
@@ -19,17 +20,31 @@ inline void _calculate_square_sfp_rows_()
     TTI_SFPLOAD(p_sfpu::LREG0, p_sfpu::sfpmem::DEFAULT, ADDR_MOD_7, 0, 0); // load from dest into lreg[0]
     // Multiply LREG0 * LREG0, store result in LREG0
     TTI_SFPMUL(p_sfpu::LREG0, p_sfpu::LREG0, p_sfpu::LCONST_0, p_sfpu::LREG0, 0);
+    TTI_SFPNOP(0, 0, 0);
     // Store result back to destination
     TTI_SFPSTORE(p_sfpu::LREG0, p_sfpu::sfpmem::DEFAULT, ADDR_MOD_7, 0, 0);
 }
 
 inline void _calculate_square_(const int iterations)
 {
-#pragma GCC unroll 8
-    for (int d = 0; d < iterations; d++)
+    int d = 0;
+    for (; d < (iterations & ~7); d += 8)
+    {
+        lltt::record<lltt::Exec>(0, 5);
+        _calculate_square_sfp_rows_<false>();
+        ckernel::math::_incr_counters_<0x0, 0x0, ckernel::math::SFP_ROWS, 0x0>();
+        lltt::replay(0, 5);
+        lltt::replay(0, 5);
+        lltt::replay(0, 5);
+        lltt::replay(0, 5);
+        lltt::replay(0, 5);
+        lltt::replay(0, 5);
+        lltt::replay(0, 5);
+    }
+    for (; d < iterations; d++)
     {
         _calculate_square_sfp_rows_<false>();
-        ckernel::math::_incr_counters_<0x0, 0x0, ckernel::math::SFP_ROWS, 0x0>(); // does the dest_reg++ (increments by 2 rows)
+        ckernel::math::_incr_counters_<0x0, 0x0, ckernel::math::SFP_ROWS, 0x0>();
     }
 }
 
