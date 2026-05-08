a02046b3d7cb24bab28e2c1729b478d585eed5db
Ana Mokan <amokan@tenstorrent.com>
2026-05-08 13:24:16 +0000

    Add l1_acc compute api for Quasar and run test on Quasar

--- tests/tt_metal/tt_metal/test_kernels/compute/unit_tests/matmul/reader_binary_blocked.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/unit_tests/matmul/reader_binary_blocked.cpp
@@ -5,9 +5,13 @@
 #include <stdint.h>
 
 #include "api/dataflow/dataflow_api.h"
-#include "experimental/circular_buffer.h"
 #include "experimental/endpoints.h"
 #include "experimental/noc.h"
+#ifdef ARCH_QUASAR
+#include "experimental/dataflow_buffer.h"
+#else
+#include "experimental/circular_buffer.h"
+#endif
 
 void kernel_main() {
     const uint32_t in0_cb = get_compile_time_arg_val(0);
@@ -23,8 +27,13 @@ void kernel_main() {
     uint32_t in0_block_size_bytes = get_arg_val<uint32_t>(7);
     uint32_t in1_block_size_bytes = get_arg_val<uint32_t>(8);
 
-    experimental::CircularBuffer cb0(in0_cb);
-    experimental::CircularBuffer cb1(in1_cb);
+#ifdef ARCH_QUASAR
+    experimental::DataflowBuffer cb_in0(in0_cb);
+    experimental::DataflowBuffer cb_in1(in1_cb);
+#else
+    experimental::CircularBuffer cb_in0(in0_cb);
+    experimental::CircularBuffer cb_in1(in1_cb);
+#endif
     experimental::Noc noc;
     experimental::AllocatorBank<experimental::AllocatorBankType::DRAM> dram_src0;
     experimental::AllocatorBank<experimental::AllocatorBankType::DRAM> dram_src1;
@@ -36,16 +45,16 @@ void kernel_main() {
         uint64_t src0_noc_addr = get_noc_addr_from_bank_id<true>(src0_dram_bank_id, src0_addr);
         uint64_t src1_noc_addr = get_noc_addr_from_bank_id<true>(src1_dram_bank_id, src1_addr);
 
-        cb0.reserve_back(in0_block_tile_cnt);
-        cb1.reserve_back(in1_block_tile_cnt);
+        cb_in0.reserve_back(in0_block_tile_cnt);
+        cb_in1.reserve_back(in1_block_tile_cnt);
 
-        noc.async_read(dram_src0, cb0, in0_block_size_bytes, {.bank_id = src0_dram_bank_id, .addr = src0_addr}, {});
-        noc.async_read(dram_src1, cb1, in1_block_size_bytes, {.bank_id = src1_dram_bank_id, .addr = src1_addr}, {});
+        noc.async_read(dram_src0, cb_in0, in0_block_size_bytes, {.bank_id = src0_dram_bank_id, .addr = src0_addr}, {});
+        noc.async_read(dram_src1, cb_in1, in1_block_size_bytes, {.bank_id = src1_dram_bank_id, .addr = src1_addr}, {});
 
         noc.async_read_barrier();
 
-        cb0.push_back(in0_block_tile_cnt);
-        cb1.push_back(in1_block_tile_cnt);
+        cb_in0.push_back(in0_block_tile_cnt);
+        cb_in1.push_back(in1_block_tile_cnt);
 
         src0_addr += in0_block_size_bytes;
         src1_addr += in1_block_size_bytes;
--- tests/tt_metal/tt_metal/test_kernels/compute/unit_tests/matmul/writer_unary.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/unit_tests/matmul/writer_unary.cpp
@@ -3,9 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "api/dataflow/dataflow_api.h"
-#include "experimental/circular_buffer.h"
 #include "experimental/endpoints.h"
 #include "experimental/noc.h"
+#ifdef ARCH_QUASAR
+#include "experimental/dataflow_buffer.h"
+#else
+#include "experimental/circular_buffer.h"
+#endif
 
 void kernel_main() {
     const uint32_t out_cb = get_compile_time_arg_val(0);
@@ -15,21 +19,25 @@ void kernel_main() {
 
     // single-tile ublocks
 
-    experimental::CircularBuffer cb(out_cb);
+#ifdef ARCH_QUASAR
+    experimental::DataflowBuffer cb_out(out_cb);
+    uint32_t ublock_size_bytes = cb_out.get_entry_size();
+#else
+    experimental::CircularBuffer cb_out(out_cb);
+    uint32_t ublock_size_bytes = cb_out.get_tile_size();
+#endif
+    uint32_t ublock_size_tiles = 1;
     experimental::Noc noc;
     experimental::AllocatorBank<experimental::AllocatorBankType::DRAM> dram_dst;
 
-    uint32_t ublock_size_bytes = cb.get_tile_size();
-    uint32_t ublock_size_tiles = 1;
-
     for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
-        cb.wait_front(ublock_size_tiles);
+        cb_out.wait_front(ublock_size_tiles);
 
-        noc.async_write(cb, dram_dst, ublock_size_bytes, {}, {.bank_id = dst_dram_bank_id_addr, .addr = dst_addr});
+        noc.async_write(cb_out, dram_dst, ublock_size_bytes, {}, {.bank_id = dst_dram_bank_id_addr, .addr = dst_addr});
 
         noc.async_write_barrier();
 
-        cb.pop_front(ublock_size_tiles);
+        cb_out.pop_front(ublock_size_tiles);
         dst_addr += ublock_size_bytes;
     }
 }
