0d5fffa8840ba14a1efd2ad19cdfd63a73992725
Alexander Nash <anash@tenstorrent.com>
2026-05-08 18:24:14 -0500

    Added tracy zones to all the unit tests

--- tests/tt_metal/tt_metal/api/test_duplicate_kernel.cpp
+++ tests/tt_metal/tt_metal/api/test_duplicate_kernel.cpp
@@ -18,6 +18,7 @@
 #include <tt-metalium/device.hpp>
 #include <tt-metalium/dispatch_core_common.hpp>
 #include "mesh_dispatch_fixture.hpp"
+#include <tracy/Tracy.hpp>
 #include <tt-metalium/distributed.hpp>
 #include "gtest/gtest.h"
 #include <tt-metalium/program.hpp>
@@ -30,57 +31,206 @@ using namespace tt;
 
 // Ensures we cannot create duplicate kernels
 TEST_F(MeshDispatchFixture, TensixFailOnDuplicateKernelCreationDataflow) {
+    ZoneScopedN("TensixFailOnDuplicateKernelCreationDataflow");
     for (const auto& device : this->devices_) {
+        ZoneScopedN("TensixFailOnDuplicateKernelCreationDataflow device");
         distributed::MeshWorkload workload;
         auto zero_coord = distributed::MeshCoordinate(0, 0);
         auto device_range = distributed::MeshCoordinateRange(zero_coord, zero_coord);
-        tt_metal::Program program = CreateProgram();
-        workload.add_program(device_range, std::move(program));
-        auto& program_ = workload.get_programs().at(device_range);
+        tt_metal::Program program = [&] {
+            ZoneScopedN("Dataflow create program");
+            return CreateProgram();
+        }();
+        {
+            ZoneScopedN("Dataflow add program to workload");
+            workload.add_program(device_range, std::move(program));
+        }
+        auto& program_ = [&]() -> tt_metal::Program& {
+            ZoneScopedN("Dataflow get program from workload");
+            return workload.get_programs().at(device_range);
+        }();
 
-        CoreCoord compute_grid = device->compute_with_storage_grid_size();
+        CoreCoord compute_grid = [&] {
+            ZoneScopedN("Dataflow compute grid size");
+            return device->compute_with_storage_grid_size();
+        }();
         EXPECT_THROW(
             {
-                tt_metal::CreateKernel(
-                    program_,
-                    "tests/tt_metal/tt_metal/test_kernels/dataflow/dram_copy.cpp",
-                    CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
-                    DataMovementConfig{
-                        .processor = tt_metal::DataMovementProcessor::RISCV_0, .noc = tt_metal::NOC::RISCV_0_default});
-                tt_metal::CreateKernel(
-                    program_,
-                    "tests/tt_metal/tt_metal/test_kernels/dataflow/dram_copy.cpp",
-                    CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
-                    DataMovementConfig{
-                        .processor = tt_metal::DataMovementProcessor::RISCV_0, .noc = tt_metal::NOC::RISCV_0_default});
+                ZoneScopedN("Dataflow duplicate creation expect throw body");
+                {
+                    ZoneScopedN("Dataflow create first dram_copy kernel");
+                    tt_metal::CreateKernel(
+                        program_,
+                        "tests/tt_metal/tt_metal/test_kernels/dataflow/dram_copy.cpp",
+                        CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
+                        DataMovementConfig{
+                            .processor = tt_metal::DataMovementProcessor::RISCV_0,
+                            .noc = tt_metal::NOC::RISCV_0_default});
+                }
+                {
+                    ZoneScopedN("Dataflow create duplicate dram_copy kernel");
+                    tt_metal::CreateKernel(
+                        program_,
+                        "tests/tt_metal/tt_metal/test_kernels/dataflow/dram_copy.cpp",
+                        CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
+                        DataMovementConfig{
+                            .processor = tt_metal::DataMovementProcessor::RISCV_0,
+                            .noc = tt_metal::NOC::RISCV_0_default});
+                }
             },
             std::exception);
     }
 }
 
 TEST_F(MeshDispatchFixture, TensixFailOnDuplicateKernelCreationCompute) {
+    ZoneScopedN("TensixFailOnDuplicateKernelCreationCompute");
     for (const auto& device : this->devices_) {
+        ZoneScopedN("TensixFailOnDuplicateKernelCreationCompute device");
         distributed::MeshWorkload workload;
         auto zero_coord = distributed::MeshCoordinate(0, 0);
         auto device_range = distributed::MeshCoordinateRange(zero_coord, zero_coord);
-        tt_metal::Program program = CreateProgram();
-        workload.add_program(device_range, std::move(program));
-        auto& program_ = workload.get_programs().at(device_range);
+        tt_metal::Program program = [&] {
+            ZoneScopedN("Compute create program");
+            return CreateProgram();
+        }();
+        {
+            ZoneScopedN("Compute add program to workload");
+            workload.add_program(device_range, std::move(program));
+        }
+        auto& program_ = [&]() -> tt_metal::Program& {
+            ZoneScopedN("Compute get program from workload");
+            return workload.get_programs().at(device_range);
+        }();
 
-        CoreCoord compute_grid = device->compute_with_storage_grid_size();
+        CoreCoord compute_grid = [&] {
+            ZoneScopedN("Compute compute grid size");
+            return device->compute_with_storage_grid_size();
+        }();
         std::vector<uint32_t> compute_kernel_args = {};
         EXPECT_THROW(
             {
+                ZoneScopedN("Compute duplicate creation expect throw body");
+                {
+                    ZoneScopedN("Compute create broadcast kernel");
+                    tt_metal::CreateKernel(
+                        program_,
+                        "tests/tt_metal/tt_metal/test_kernels/compute/broadcast.cpp",
+                        CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
+                        ComputeConfig{
+                            .math_fidelity = MathFidelity::HiFi4,
+                            .fp32_dest_acc_en = false,
+                            .math_approx_mode = false,
+                            .compile_args = compute_kernel_args,
+                            .opt_level = KernelBuildOptLevel::O3});
+                }
+                {
+                    ZoneScopedN("Compute create duplicate matmul kernel");
+                    tt_metal::CreateKernel(
+                        program_,
+                        "tests/tt_metal/tt_metal/test_kernels/compute/matmul.cpp",
+                        CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
+                        ComputeConfig{
+                            .math_fidelity = MathFidelity::HiFi4,
+                            .fp32_dest_acc_en = false,
+                            .math_approx_mode = false,
+                            .compile_args = compute_kernel_args,
+                            .opt_level = KernelBuildOptLevel::O3});
+                }
+            },
+            std::exception);
+    }
+}
+
+TEST_F(MeshDispatchFixture, TensixPassOnNormalKernelCreation) {
+    ZoneScopedN("TensixPassOnNormalKernelCreation");
+    for ([[maybe_unused]] const auto& mesh_device : this->devices_) {
+        ZoneScopedN("TensixPassOnNormalKernelCreation device");
+        distributed::MeshWorkload workload;
+        auto zero_coord = distributed::MeshCoordinate(0, 0);
+        auto device_range = distributed::MeshCoordinateRange(zero_coord, zero_coord);
+        tt_metal::Program program = [&] {
+            ZoneScopedN("Normal create program");
+            return CreateProgram();
+        }();
+        {
+            ZoneScopedN("Normal add program to workload");
+            workload.add_program(device_range, std::move(program));
+        }
+        auto& program_ = [&]() -> tt_metal::Program& {
+            ZoneScopedN("Normal get program from workload");
+            return workload.get_programs().at(device_range);
+        }();
+        std::vector<uint32_t> compute_kernel_args = {};
+        EXPECT_NO_THROW({
+            ZoneScopedN("Normal creation expect no throw body");
+            {
+                ZoneScopedN("Normal create broadcast kernel");
                 tt_metal::CreateKernel(
                     program_,
                     "tests/tt_metal/tt_metal/test_kernels/compute/broadcast.cpp",
-                    CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
+                    CoreCoord(1, 0),
                     ComputeConfig{
                         .math_fidelity = MathFidelity::HiFi4,
                         .fp32_dest_acc_en = false,
                         .math_approx_mode = false,
                         .compile_args = compute_kernel_args,
                         .opt_level = KernelBuildOptLevel::O3});
+            }
+            {
+                ZoneScopedN("Normal create matmul kernel");
+                tt_metal::CreateKernel(
+                    program_,
+                    "tests/tt_metal/tt_metal/test_kernels/compute/matmul.cpp",
+                    CoreCoord(0, 0),
+                    ComputeConfig{
+                        .math_fidelity = MathFidelity::HiFi4,
+                        .fp32_dest_acc_en = false,
+                        .math_approx_mode = false,
+                        .compile_args = compute_kernel_args,
+                        .opt_level = KernelBuildOptLevel::O3});
+            }
+        });
+    }
+}
+
+TEST_F(MeshDispatchFixture, TensixPassOnMixedOverlapKernelCreation) {
+    ZoneScopedN("TensixPassOnMixedOverlapKernelCreation");
+    for (const auto& device : this->devices_) {
+        ZoneScopedN("TensixPassOnMixedOverlapKernelCreation device");
+        distributed::MeshWorkload workload;
+        auto zero_coord = distributed::MeshCoordinate(0, 0);
+        auto device_range = distributed::MeshCoordinateRange(zero_coord, zero_coord);
+        tt_metal::Program program = [&] {
+            ZoneScopedN("Mixed overlap create program");
+            return CreateProgram();
+        }();
+        {
+            ZoneScopedN("Mixed overlap add program to workload");
+            workload.add_program(device_range, std::move(program));
+        }
+        auto& program_ = [&]() -> tt_metal::Program& {
+            ZoneScopedN("Mixed overlap get program from workload");
+            return workload.get_programs().at(device_range);
+        }();
+        CoreCoord compute_grid = [&] {
+            ZoneScopedN("Mixed overlap compute grid size");
+            return device->compute_with_storage_grid_size();
+        }();
+        std::vector<uint32_t> compute_kernel_args = {};
+        EXPECT_NO_THROW({
+            ZoneScopedN("Mixed overlap creation expect no throw body");
+            {
+                ZoneScopedN("Mixed overlap create dram_copy kernel");
+                tt_metal::CreateKernel(
+                    program_,
+                    "tests/tt_metal/tt_metal/test_kernels/dataflow/dram_copy.cpp",
+                    CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
+                    DataMovementConfig{
+                        .processor = tt_metal::DataMovementProcessor::RISCV_0,
+                        .noc = tt_metal::NOC::RISCV_0_default});
+            }
+            {
+                ZoneScopedN("Mixed overlap create matmul kernel");
                 tt_metal::CreateKernel(
                     program_,
                     "tests/tt_metal/tt_metal/test_kernels/compute/matmul.cpp",
@@ -91,72 +241,7 @@ TEST_F(MeshDispatchFixture, TensixFailOnDuplicateKernelCreationCompute) {
                         .math_approx_mode = false,
                         .compile_args = compute_kernel_args,
                         .opt_level = KernelBuildOptLevel::O3});
-            },
-            std::exception);
-    }
-}
-
-TEST_F(MeshDispatchFixture, TensixPassOnNormalKernelCreation) {
-    for ([[maybe_unused]] const auto& mesh_device : this->devices_) {
-        distributed::MeshWorkload workload;
-        auto zero_coord = distributed::MeshCoordinate(0, 0);
-        auto device_range = distributed::MeshCoordinateRange(zero_coord, zero_coord);
-        tt_metal::Program program = CreateProgram();
-        workload.add_program(device_range, std::move(program));
-        auto& program_ = workload.get_programs().at(device_range);
-        std::vector<uint32_t> compute_kernel_args = {};
-        EXPECT_NO_THROW({
-            tt_metal::CreateKernel(
-                program_,
-                "tests/tt_metal/tt_metal/test_kernels/compute/broadcast.cpp",
-                CoreCoord(1, 0),
-                ComputeConfig{
-                    .math_fidelity = MathFidelity::HiFi4,
-                    .fp32_dest_acc_en = false,
-                    .math_approx_mode = false,
-                    .compile_args = compute_kernel_args,
-                    .opt_level = KernelBuildOptLevel::O3});
-            tt_metal::CreateKernel(
-                program_,
-                "tests/tt_metal/tt_metal/test_kernels/compute/matmul.cpp",
-                CoreCoord(0, 0),
-                ComputeConfig{
-                    .math_fidelity = MathFidelity::HiFi4,
-                    .fp32_dest_acc_en = false,
-                    .math_approx_mode = false,
-                    .compile_args = compute_kernel_args,
-                    .opt_level = KernelBuildOptLevel::O3});
-        });
-    }
-}
-
-TEST_F(MeshDispatchFixture, TensixPassOnMixedOverlapKernelCreation) {
-    for (const auto& device : this->devices_) {
-        distributed::MeshWorkload workload;
-        auto zero_coord = distributed::MeshCoordinate(0, 0);
-        auto device_range = distributed::MeshCoordinateRange(zero_coord, zero_coord);
-        tt_metal::Program program = CreateProgram();
-        workload.add_program(device_range, std::move(program));
-        auto& program_ = workload.get_programs().at(device_range);
-        CoreCoord compute_grid = device->compute_with_storage_grid_size();
-        std::vector<uint32_t> compute_kernel_args = {};
-        EXPECT_NO_THROW({
-            tt_metal::CreateKernel(
-                program_,
-                "tests/tt_metal/tt_metal/test_kernels/dataflow/dram_copy.cpp",
-                CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
-                DataMovementConfig{
-                    .processor = tt_metal::DataMovementProcessor::RISCV_0, .noc = tt_metal::NOC::RISCV_0_default});
-            tt_metal::CreateKernel(
-                program_,
-                "tests/tt_metal/tt_metal/test_kernels/compute/matmul.cpp",
-                CoreRange(CoreCoord(0, 0), CoreCoord(compute_grid.x, compute_grid.y)),
-                ComputeConfig{
-                    .math_fidelity = MathFidelity::HiFi4,
-                    .fp32_dest_acc_en = false,
-                    .math_approx_mode = false,
-                    .compile_args = compute_kernel_args,
-                    .opt_level = KernelBuildOptLevel::O3});
+            }
         });
     }
 }
